3fde44d0c48294d30ecf5749e3838e2a06eb8f51
Reyansh <nikkibarnwal44@gmail.com>
2024-02-23 15:41:10 +0530

    Added all files for habit tracker app

---
+++ src/constants/habitStatus.js
@@ -0,0 +1,4 @@
+export const DONE = 'done';
+export const NOT_DONE = 'not_done';
+export const NONE = 'none';
+
---
+++ src/pages/Habits.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import HabitComponent from '../components/HabitComponent';
+
+const Habits = () => {
+  const habits = useSelector((state) => state.habits);
+  return (
+    <Container style={{ padding: '10px' }}>
+      {habits.map((habit) => (
+        <HabitComponent habit={habit} key={habit.title} />
+      ))}
+    </Container>
+  );
+};
+
+export default Habits;
